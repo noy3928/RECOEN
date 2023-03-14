fcb5dca8406293ca6e594d13c30be75daf3aa303
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-03-14 16:39:43 +0900

    fix : remove sentry

--- next.config.js
+++ next.config.js
@@ -13,9 +13,9 @@ const nextConfig = {
     domains: ['recoen.s3.ap-northeast-2.amazonaws.com'],
     formats: ['image/avif', 'image/webp'],
   },
-  sentry: {
-    hideSourceMaps: true,
-  },
+  // sentry: {
+  //   hideSourceMaps: true,
+  // },
   // webpack: (config, options) => {
   //   config.plugins.push(
   //     new options.webpack.DefinePlugin({
